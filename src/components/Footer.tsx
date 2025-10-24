9f9811c7a5c0725526b7fd85a5ca60407da43a35
Daniel Lambert <danjlambert95@gmail.com>
2025-10-24 12:21:51 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,18 +18,10 @@ const Footer = () => {
             
             {/* Social Media Buttons */}
             <div className="flex space-x-4">
-              <a 
-                href="www.instagram.com/blackslopes" 
-                className="group bg-blue-600 hover:bg-blue-700 p-3 rounded-lg transition-all duration-300 transform hover:scale-105"
-                aria-label="Facebook"
-              >
-                <svg className="h-5 w-5 text-white" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
-                </svg>
-              </a>
-              
-              <a 
-                href="www.instagram.com/blackslopes" 
+              <a
+                href="https://www.instagram.com/blackslopes"
+                target="_blank"
+                rel="noopener noreferrer"
                 className="group bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 p-3 rounded-lg transition-all duration-300 transform hover:scale-105"
                 aria-label="Instagram"
               >
